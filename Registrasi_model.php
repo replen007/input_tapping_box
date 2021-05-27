a7a70fe05120c60d871263853a0fcc66b7695a47
replen007 <84306684+replen007@users.noreply.github.com>
2021-05-27 08:24:11 +0700

    update query registrasi
    menambahkan input foto

--- Registrasi_model.php
+++ Registrasi_model.php
@@ -25,24 +25,67 @@ class Registrasi_model extends CI_Model
 
     public function registrasi()
     {
-        $data = [
-            'nama_op'       => filter_string($this->input->post('nama_op', true)),
-            'npwpd'         => filter_string($this->input->post('npwpd', true)),
-            'nik'           => filter_string($this->input->post('nik', true)),
-            'alamat'        => filter_string($this->input->post('alamat', true)),
-            'kecamatan'     => filter_string($this->input->post('kecamatan', true)),
-            'kelurahan'     => filter_string($this->input->post('kelurahan', true)),
-            'status'        => filter_string($this->input->post('status', true)),
-            'jenis'         => filter_string($this->input->post('jenis', true)),
-            'tgl_registrasi' => time()
-        ];
+//         $data = [
+//             'nama_op'       => filter_string($this->input->post('nama_op', true)),
+//             'npwpd'         => filter_string($this->input->post('npwpd', true)),
+//             'nik'           => filter_string($this->input->post('nik', true)),
+//             'alamat'        => filter_string($this->input->post('alamat', true)),
+//             'kecamatan'     => filter_string($this->input->post('kecamatan', true)),
+//             'kelurahan'     => filter_string($this->input->post('kelurahan', true)),
+//             'status'        => filter_string($this->input->post('status', true)),
+//             'jenis'         => filter_string($this->input->post('jenis', true)),
+//             'tgl_registrasi' => time()
+//         ];
 
-        $this->db->insert('reg_registrasi', $data);
-        return $this->db->affected_rows();
+//         $this->db->insert('reg_registrasi', $data);
+//         return $this->db->affected_rows();
+        
+                $nama_op     = $this->input->post('nama_op', true);
+        $npwpd       = $this->input->post('npwpd', true);
+        $nik         = $this->input->post('nik', true);
+        $alamat      = $this->input->post('alamat', true);
+        $kecamatan   = $this->input->post('kecamatan', true);
+        $kelurahan   = $this->input->post('kelurahan', true);
+        $status      = $this->input->post('status', true);
+        $jenis       = $this->input->post('jenis', true);
+        $tgl_insert  = time();
+
+        $upload      = $this->upload->data('file_name');
+
+
+        # file_name with url
+        $location         = base_url() . 'images/foto/';
+        $insert_image     = $location . $upload;
+
+        # File_name
+        // date_default_timezone_set('Asia/Jakarta');
+        // $time_upload      = base_url() . 'images/foto/' . $nik . '-' . date('d-m-y/H:i', $tgl_insert) . 'enc';
+        // $insert_image     = $time_upload . $upload;
+
+        // date_default_timezone_set('Asia/Jakarta');
+        // # di db nama image menjadi datetime-NIK-date;
+        // $time_upload      = date('d-m-y H:i', $tgl_insert) . '-';
+        // $insert_image     = $time_upload . $upload;
+
+
+        $data = array(
+            'nama_op'    => $nama_op,
+            'npwpd'      => $npwpd,
+            'nik'        => $nik,
+            'alamat'     => $alamat,
+            'kecamatan'  => $kecamatan,
+            'kelurahan'  => $kelurahan,
+            'status'     => $status,
+            'jenis'      => $jenis,
+            'tgl_registrasi' => $tgl_insert,
+            'file'       => $insert_image
+        );
+
+        return $this->db->insert('reg_registrasi', $data);
     }
 
     public function show_registrasi()
     {
-        $this->db->select('nama_op as reg_registrasi, kecamatan as reg_kecamatan, kelurahan as reg_kelurahan');
+        //$this->db->select('nama_op as reg_registrasi, kecamatan as reg_kecamatan, kelurahan as reg_kelurahan');
     }
 }
